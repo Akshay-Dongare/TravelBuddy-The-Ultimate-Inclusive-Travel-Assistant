6137450e518c91fa551abd42c6283682dc46e606
Akshay-Dongare <akshayd02@gmai.com>
2024-01-24 12:19:26 +0530

    modified:   config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 #COHERE_API_KEY = "your-api-key"
 OPENAI_API_KEY = "your-api-key"
-PINECONE_API_KEY = "your-api-key"
+PINECONE_API_KEY = "e4a748ef-7fb2-4897-b309-c881ca7520c2"
 PINECONE_ENVIRONMENT = "your-env"
