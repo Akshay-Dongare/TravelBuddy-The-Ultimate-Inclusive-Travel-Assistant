ef596b8cdfa32b76b0a30a895c980bfa05199a28
Akshay-Dongare <akshayd02@gmai.com>
2024-01-24 19:58:03 +0530

    modified:   main2.py

--- main2.py
+++ main2.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request
-import openai
 import config
+import os
+from openai import OpenAI
 
 
 app = Flask(__name__)
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("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"),
+)
 
 @app.route("/")
 def index():
@@ -18,12 +26,13 @@ def chat():
 
 def response_by_llm(query):
     #Q&A using gpt3.5
-    completion = openai.ChatCompletion.create(
+    completion = client.chat.completions.create(
+        messages=[
+            {"role": "user", 
+             "content":f"I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with [Invalid Question]. Now, I will answer this question:{query} to the best of my knowledge."
+             }
+             ],
     model="gpt-3.5-turbo",
-    messages=[
-    {"role": "user", 
-    "content":f"I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with [Invalid Question]. Now, I will answer this question:{query} to the best of my knowledge."}
-    ]
     )
     return completion.choices[0].message.content
 
