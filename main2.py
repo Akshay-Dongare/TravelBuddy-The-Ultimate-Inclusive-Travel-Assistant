353b6ed163a2a8d2ea392a55f1abcecb07b729d0
Akshay-Dongare <akshayd02@gmai.com>
2024-01-24 21:55:15 +0530

    modified:   templates/chat.html

--- main2.py
+++ main2.py
@@ -5,52 +5,6 @@ from openai import OpenAI
 import speech_recognition as sr
 import pyttsx3 
 
-'''
-# Initialize the recognizer 
-r = sr.Recognizer() 
-
-# Function to convert text to
-# speech
-def SpeakText(command):
-     
-    # Initialize the engine
-    engine = pyttsx3.init("sapi5", True) #driver for windows and debug mode as args
-    engine.say(command) 
-    engine.runAndWait()
-
-# Loop infinitely for user to
-# speak
- 
-while(1):    
-     
-    # Exception handling to handle
-    # exceptions at the runtime
-    try:
-         
-        # use the microphone as source for input.
-        with sr.Microphone() as source2:
-             
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level 
-            r.adjust_for_ambient_noise(source2, duration=0.2)
-             
-            #listens for the user's input 
-            audio2 = r.listen(source2)
-             
-            # Using google to recognize audio
-            MyText = r.recognize_google(audio2)
-            MyText = MyText.lower()
- 
-            print("Did you say ",MyText)
-            SpeakText(MyText)
-             
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
-         
-    except sr.UnknownValueError:
-        print("unknown error occurred")
-'''
 app = Flask(__name__)
 
 os.environ["OPENAI_API_KEY"] = "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"
@@ -65,6 +19,62 @@ client = OpenAI(
 def index():
     return render_template('chat.html')
 
+@app.route('/voice',methods=["GET", "POST"])
+def voice():
+    # Initialize the recognizer 
+    r = sr.Recognizer() 
+
+    # Function to convert text to
+    # speech
+    def SpeakText(command):
+        
+        # Initialize the engine
+        engine = pyttsx3.init("sapi5", True) #driver for windows and debug mode as args
+        engine.say(command) 
+        engine.runAndWait()
+
+    # Loop infinitely for user to
+    # speak
+    
+    while(1):    
+        
+        # Exception handling to handle
+        # exceptions at the runtime
+        try:
+            
+            # use the microphone as source for input.
+            with sr.Microphone() as source2:
+                
+                # wait for a second to let the recognizer
+                # adjust the energy threshold based on
+                # the surrounding noise level 
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+                
+                #listens for the user's input 
+                audio2 = r.listen(source2)
+                
+                # Using google to recognize audio
+                MyText = r.recognize_google(audio2)
+                MyText = MyText.lower()
+    
+                print("Did you say ",MyText)
+                
+                #Q&A using gpt3.5
+                completion = client.chat.completions.create(
+                    messages=[
+                        {"role": "user", 
+                        "content":f"I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with [Invalid Question]. Now, I will answer this question:{MyText} to the best of my knowledge."
+                        }
+                        ],
+                model="gpt-3.5-turbo",
+                )
+                SpeakText(completion.choices[0].message.content)
+                
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+            
+        except sr.UnknownValueError:
+            print("unknown error occurred")
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
