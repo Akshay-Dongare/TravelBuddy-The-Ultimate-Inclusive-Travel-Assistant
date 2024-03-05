dfaa6aa4adb8906969e2ccb1723c49b6a32947f2
Akshay-Dongare <akshayd02@gmail.com>
2024-03-05 10:31:54 +0530

    modified:   ollama-streamlit.py

--- ollama-streamlit.py
+++ ollama-streamlit.py
@@ -117,5 +117,76 @@ def voice():
             
         except sr.UnknownValueError:
             print("unknown error occurred")
+def continous_voice():
+    # Toggle button on click
+    st.session_state.button = not st.session_state.button
+
+
+    # Initialize the recognizer 
+    r = sr.Recognizer() 
+
+    # Function to convert text to
+    # speech
+    def SpeakText(command):
+        
+        # Initialize the engine
+        engine = pyttsx3.init("sapi5", True) #driver for windows and debug mode as args
+        engine.say(command) 
+        engine.runAndWait()
+
+    # Loop infinitely for user to
+    # speak
+    
+    while(1):
+        
+        # Exception handling to handle
+        # exceptions at the runtime
+        try:
+            
+            # use the microphone as source for input.
+            with sr.Microphone() as source2:
+                
+                # wait for a second to let the recognizer
+                # adjust the energy threshold based on
+                # the surrounding noise level 
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+                
+                #listens for the user's input 
+                audio2 = r.listen(source2)
+                
+                # Using google to recognize audio
+                MyText = r.recognize_google(audio2)
+                MyText = MyText.lower()
+    
+                print("Did you say ",MyText)
+
+                if (MyText=='exit'):
+                    break
+
+                # add latest message to history in format {role, content}
+                st.session_state["messages"].append({"role": "user", "content": MyText})
+
+                with st.chat_message("user"):
+                    st.markdown(MyText)
+
+                with st.chat_message("assistant"):
+                    # Without Threading
+                    SpeakText("Please give me a minute to get back to you!")
+                    message = st.write_stream(model_res_generator())
+                    SpeakText(message)
+                        
+                    st.session_state["messages"].append({"role": "assistant", "content": message})
+                    # Toggle Button
+                    #st.session_state.button = not st.session_state.button
+                
+                
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+            
+        except sr.UnknownValueError:
+            print("unknown error occurred")
+
+
 
 st.button('Mic', on_click=voice)
+st.button('Continous Voice', on_click=continous_voice)
